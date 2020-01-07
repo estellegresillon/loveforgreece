7475b918dd06db85256b93231ca9a77698e5c75d
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-07 23:10:17 +0100

    fixed mix-blend-mode and arrow

--- src/App.js
+++ src/App.js
@@ -7,25 +7,28 @@ const App = () => {
     const windowHeight = window.innerHeight;
     const cursor = document.getElementById("cursor");
     const arrow = document.querySelector(".fa-long-arrow-alt-down");
+    const coloredCursor = document.getElementById("colored-cursor");
     const top = document.querySelector(".app-layout").scrollTop;
-    const x = e.clientX;
-    const y = e.clientY;
+    const x = e.clientX + 170;
+    const y = e.clientY + 170;
+    const xbis = x - 170;
+    const ybis = y - 170;
 
     if (typeof x !== 'undefined') {
       cursor.style.opacity = 1;
       cursor.style.left = x + "px";
       cursor.style.top = y + "px";
+      arrow.style.left = x + "px";
+      arrow.style.top = y + "px";
 
       if (top === 0) {
-        // only show arrow on the bottom of the hero screen
-        // disable it above and under
-        if (windowHeight / y < 3) {
-          arrow.style.opacity = 1;
-          arrow.style.left = x + "px";
-          arrow.style.top = y + "px";
-        } else arrow.style.opacity = 0;
+        arrow.style.opacity = 1;
+        coloredCursor.style.opacity = 1;
+        coloredCursor.style.left = xbis + "px";
+        coloredCursor.style.top = ybis + "px";
       } else {
         arrow.style.opacity = 0;
+        coloredCursor.style.opacity = 0;
       }
     }
   };
@@ -34,6 +37,7 @@ const App = () => {
     const windowHeight = window.innerHeight;
     const menu = document.querySelector(".header");
     const cursor = document.getElementById("cursor");
+    const coloredCursor = document.getElementById("colored-cursor");
     const arrow = document.querySelector(".fa-long-arrow-alt-down");
 
     if (e.target.scrollTop > 0) {
@@ -42,17 +46,17 @@ const App = () => {
       menu.classList.remove("sticky");
     }
 
-    if (e.target.scrollTop > ((windowHeight / 2) - 200)) {
+    if (e.target.scrollTop > ((windowHeight / 2))) {
       cursor.style.height = "10px";
       cursor.style.width = "10px";
-      cursor.style.mixBlendMode = "screen";
       // disable arrow if scroll but no mouse move
       arrow.style.opacity = 0;
+      coloredCursor.style.opacity = 0;
     } else {
       cursor.style.height = "50px";
       cursor.style.width = "50px";
-      cursor.style.mixBlendMode = "screen";
       arrow.style.opacity = 1;
+      coloredCursor.style.opacity = 1;
     }
   }
 
@@ -69,6 +73,10 @@ const App = () => {
   return (
     <>
       <div id="cursor" />
+      <div id="colored-cursor" />
+      <div className="blue-block" />
+      <div className="red-block" />
+      <div className="yellow-block" />
       <i className="fas fa-long-arrow-alt-down" />
       <div className="app-layout">
         <div className="header">
@@ -77,11 +85,6 @@ const App = () => {
         <div className="hero-scene">
           <h1>Visit Santorini</h1>
         </div>
-        <div className="blue-block" />
-        <div className="red-block" />
-        <div className="yellow-block" />
-        
-
         <section>
           hi
         </section>
