0625f61786646a0d723d0d81e61aef03ad7b352b
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-07 21:25:19 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
     const windowHeight = window.innerHeight;
     const cursor = document.getElementById("cursor");
     const arrow = document.querySelector(".fa-long-arrow-alt-down");
+    const top = document.querySelector(".app-layout").scrollTop;
     const x = e.clientX;
     const y = e.clientY;
 
@@ -15,11 +16,17 @@ const App = () => {
       cursor.style.left = x + "px";
       cursor.style.top = y + "px";
 
-      if (windowHeight / y < 2.5) {
-        arrow.style.opacity = 1;
-        arrow.style.left = x + "px";
-        arrow.style.top = y + "px";
-      } else arrow.style.opacity = 0;
+      if (top === 0) {
+        // only show arrow on the bottom of the hero screen
+        // disable it above and under
+        if (windowHeight / y < 3) {
+          arrow.style.opacity = 1;
+          arrow.style.left = x + "px";
+          arrow.style.top = y + "px";
+        } else arrow.style.opacity = 0;
+      } else {
+        arrow.style.opacity = 0;
+      }
     }
   };
 
@@ -27,18 +34,25 @@ const App = () => {
     const windowHeight = window.innerHeight;
     const menu = document.querySelector(".header");
     const cursor = document.getElementById("cursor");
+    const arrow = document.querySelector(".fa-long-arrow-alt-down");
 
     if (e.target.scrollTop > 0) {
       menu.classList.add("sticky");
-    } else menu.classList.remove("sticky");
+    } else {
+      menu.classList.remove("sticky");
+    }
 
-    if (e.target.scrollTop > (windowHeight / 2)) {
+    if (e.target.scrollTop > ((windowHeight / 2) - 200)) {
       cursor.style.height = "10px";
       cursor.style.width = "10px";
       cursor.style.mixBlendMode = "screen";
+      // disable arrow if scroll but no mouse move
+      arrow.style.opacity = 0;
     } else {
       cursor.style.height = "50px";
       cursor.style.width = "50px";
+      cursor.style.mixBlendMode = "normal";
+      arrow.style.opacity = 1;
     }
   }
 
@@ -55,6 +69,7 @@ const App = () => {
   return (
     <>
       <div id="cursor" />
+      <i className="fas fa-long-arrow-alt-down" />
       <div className="app-layout">
         <div className="header">
           <div className="logo">LoveForGreece</div>
@@ -66,7 +81,6 @@ const App = () => {
         <div className="red-block" />
         <div className="yellow-block" />
         <img src="/home.jpg" alt="santorini-home" className="image-block" />
-        <i className="fas fa-long-arrow-alt-down" />
 
         <section>
           hi
