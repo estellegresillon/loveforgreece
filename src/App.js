d26a2108566103dd3521d79a92c3aa3f07ade309
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-07 07:28:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "./app.scss";
+
+const App = () => {
+  const setCursor = e => {
+    const windowHeight = window.innerHeight;
+    const cursor = document.getElementById("cursor");
+    const arrow = document.querySelector(".fa-long-arrow-alt-down");
+    const x = e.clientX;
+    const y = e.clientY;  
+
+    if ( typeof x !== 'undefined' ){
+      cursor.style.left = x + "px";
+      cursor.style.top = y + "px";
+
+      if (windowHeight / y < 2) {
+        arrow.style.opacity = 1;
+        arrow.style.left = x + "px";
+        arrow.style.top = y + "px";
+      } else arrow.style.opacity = 0;
+    }
+  };
+
+  const setMenuColor = e => {
+    const menu = document.querySelector(".header");
+
+    if (e.target.scrollTop > 0) {
+      menu.classList.add("sticky");
+    } else menu.classList.remove("sticky");
+  }
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", setCursor, false);
+    return () => window.removeEventListener("mousemove", setCursor, false);
+  }, []);
+
+  React.useEffect(() => {
+    document.addEventListener("scroll", setMenuColor, true);
+    return () => document.removeEventListener("scroll", setMenuColor, true);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="cursor" />
+      <div className="app-layout">
+        <div className="header">
+          <div className="logo">Logo</div>
+        </div>
+        <div className="hero-scene">
+          <h1>Hero scene message</h1>
+        </div>
+        <div className="blue-block" />
+        <div className="red-block" />
+        <div className="yellow-block" />
+        <i className="fas fa-long-arrow-alt-down" />
+        
+        <section>
+          hi
+        </section>
+        
+      </div>
+    </>
   );
 }
 
