1b8f372a12c01e89a59abe4b94342f4dd30a14df
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-08 22:59:07 +0100

    cleaned app.js

--- src/App.js
+++ src/App.js
@@ -1,76 +1,107 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-import "./app.scss";
+import "./styles/app.scss";
+
+const usePrevious = value => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 const App = () => {
-  const setCursorMove = e => {
-    const container = document.getElementById("container");
-    const circle = document.querySelector(".circle");
-    var relX = e.pageX - container.offsetLeft - 230;
-    var relY = e.pageY - container.offsetTop - 230;
-    circle.style.left = relX + "px";
-    circle.style.top = relY + "px";
+  //////////// SCROLL
+  const [count, setCount] = useState(0);
+  const prevCount = usePrevious(count)
+  
+  useEffect(() => {
+    const section = document.getElementById("section-one");
+    const sectionBis = document.getElementById("section-two");
+    if (count > prevCount) {
+      section.style.transform = "rotate(-8deg)";
+      sectionBis.style.transform = "rotate(-8deg)";
+    } else if (count < prevCount) {
+      section.style.transform = "rotate(8deg)";
+      sectionBis.style.transform = "rotate(8deg)";
+    }
+  }, [count, prevCount]);
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => { setCount(window.pageYOffset) }, true);
+    return () => document.removeEventListener("scroll", () => { setCount(window.pageYOffset) }, true);
+  }, []);
+
+  //////////// CURSORS
+  const setCursorSmall = e => {
+    const app = document.querySelector(".app");
+    const cursorSmall = document.querySelector(".cursor-small");
+    var relX = e.pageX - app.offsetLeft - 5;
+    var relY = e.pageY - app.offsetTop - 5;
+    cursorSmall.style.left = relX + "px";
+    cursorSmall.style.top = relY + "px";
   };
 
   const setCursor = e => {
-    const container = document.querySelector(".app");
-    const circleSmall = document.querySelector(".circle-small");
-    var relX = e.pageX - container.offsetLeft - 5;
-    var relY = e.pageY - container.offsetTop - 5;
-    circleSmall.style.left = relX + "px";
-    circleSmall.style.top = relY + "px";
+    const heroScene = document.getElementById("hero-scene");
+    const cursor = document.querySelector(".cursor");
+    var relX = e.pageX - heroScene.offsetLeft - 230;
+    var relY = e.pageY - heroScene.offsetTop - 230;
+    cursor.style.left = relX + "px";
+    cursor.style.top = relY + "px";
   };
 
-  const toggleCursor = (circle, number) => {
-    circle.style.opacity = number;
-  }
+  const toggleCursor = (cursor, number) => {
+    cursor.style.opacity = number;
+  };
 
   useEffect(() => {
-    const container = document.querySelector(".app");
-    container.addEventListener("mousemove", setCursor, false);
-    return () => container.removeEventListener("mousemove", setCursor, false);
+    const app = document.querySelector(".app");
+    app.addEventListener("mousemove", setCursorSmall, false);
+    return () => app.removeEventListener("mousemove", setCursorSmall, false);
   }, []);
 
   useEffect(() => {
-    const container = document.getElementById("container");
-    container.addEventListener("mousemove", setCursorMove, false);
-    return () => container.removeEventListener("mousemove", setCursorMove, false);
+    const heroScene = document.getElementById("hero-scene");
+    heroScene.addEventListener("mousemove", setCursor, false);
+    return () => heroScene.removeEventListener("mousemove", setCursor, false);
   }, []);
 
   useEffect(() => {
-    const container = document.getElementById("container");
-    const circle = document.querySelector(".circle");
-    container.addEventListener("pointerenter", () => { toggleCursor(circle, 1) });
-    return () => container.addEventListener("pointerenter", () => { toggleCursor(circle, 1) });
+    const heroScene = document.getElementById("hero-scene");
+    const cursor = document.querySelector(".cursor");
+    heroScene.addEventListener("pointerenter", () => { toggleCursor(cursor, 1) });
+    return () => heroScene.addEventListener("pointerenter", () => { toggleCursor(cursor, 1) });
   }, []);
 
   useEffect(() => {
-    const container = document.getElementById("container");
-    const circle = document.querySelector(".circle");
-    container.addEventListener("pointerleave", () => { toggleCursor(circle, 0) });
-    container.addEventListener("pointerleave", () => { toggleCursor(circle, 0) });
+    const heroScene = document.getElementById("hero-scene");
+    const cursor = document.querySelector(".cursor");
+    heroScene.addEventListener("pointerleave", () => { toggleCursor(cursor, 0) });
+    heroScene.addEventListener("pointerleave", () => { toggleCursor(cursor, 0) });
   }, []);
 
   return (
     <div className="app">
-      <div className="blue-block" />
-      <div className="red-block" />
-      <div className="yellow-block" />
+      <div className="mount-animation-light-blue" />
+      <div className="mount-animation-blue" />
+      <div className="mount-animation-yellow" />
 
-      <div className="circle-small"></div>
+      <div className="cursor-small"></div>
       <div className="header"></div>
 
-      <div id="container">
-        <div className="circle"></div>
+      <div id="hero-scene">
+        <div className="cursor"></div>
         <div className="message">Visit Santorini</div>
         <div className="message-stroke">- The Cyclades & the Egee sea -</div>
       </div>
 
-      <div id="picture">
+      <div id="section-one">
         <div className="message-stroke">Hello</div>
       </div>
 
-      <div id="container">
+      <div id="section-two">
         <div className="message-stroke">Hello</div>
       </div>
     </div>
