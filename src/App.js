8622e44231d1a0e78d4f48824d2a50f43ab53479
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-08 22:13:13 +0100

    fixed scroll and pointer

--- src/App.js
+++ src/App.js
@@ -1,121 +1,79 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 
 import "./app.scss";
 
-function usePrevious(value) {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  });
-  return ref.current;
-}
-
 const App = () => {
-  const [count, setCount] = useState(0);
-  const prevCount = usePrevious(count)
-
   const setCursorMove = e => {
-    const cursor = document.getElementById("cursor");
-    const arrow = document.querySelector(".fa-long-arrow-alt-down");
-    const coloredCursor = document.getElementById("colored-cursor");
-    const top = document.querySelector(".app-layout").scrollTop;
-    const x = e.clientX ;
-    const y = e.clientY;
-    const xbis = x - 170;
-    const ybis = y - 170;
-
-    if (typeof x !== 'undefined') {
-      cursor.style.opacity = 1;
-      cursor.style.left = x + "px";
-      cursor.style.top = y + "px";
-      arrow.style.left = x + "px";
-      arrow.style.top = y + "px";
-
-      if (top === 0) {
-        arrow.style.opacity = 1;
-        coloredCursor.style.opacity = 1;
-        coloredCursor.style.left = xbis + "px";
-        coloredCursor.style.top = ybis + "px";
-      } else {
-        arrow.style.opacity = 0;
-        coloredCursor.style.opacity = 0;
-      }
-    }
+    const container = document.getElementById("container");
+    const circle = document.querySelector(".circle");
+    var relX = e.pageX - container.offsetLeft - 230;
+    var relY = e.pageY - container.offsetTop - 230;
+    circle.style.left = relX + "px";
+    circle.style.top = relY + "px";
   };
 
-  // const setCursorScroll = e => {
-  //   const windowHeight = window.innerHeight;
-  //   const cursor = document.getElementById("cursor");
-  //   const coloredCursor = document.getElementById("colored-cursor");
-  //   const arrow = document.querySelector(".fa-long-arrow-alt-down");
-  //   setCount(e.target.scrollTop)
+  const setCursor = e => {
+    const container = document.querySelector(".app");
+    const circleSmall = document.querySelector(".circle-small");
+    var relX = e.pageX - container.offsetLeft - 5;
+    var relY = e.pageY - container.offsetTop - 5;
+    circleSmall.style.left = relX + "px";
+    circleSmall.style.top = relY + "px";
+  };
 
-  //   console.log("hi")
-  //   coloredCursor.style.opacity = 0;
-  //   coloredCursor.style.left = 0 + "px";
-  //   coloredCursor.style.top = 0 + "px";
-  
-  //   if (e.target.scrollTop > ((windowHeight / 2))) {
-  //     cursor.style.height = "10px";
-  //     cursor.style.width = "10px";
-  //     arrow.style.opacity = 0;
-  //     //coloredCursor.style.opacity = 0;
-  //   } else {
-  //     cursor.style.height = "50px";
-  //     cursor.style.width = "50px";
-  //     arrow.style.opacity = 1;
-  //    // coloredCursor.style.opacity = 1;
-  //   }
-  // }
+  const toggleCursor = (circle, number) => {
+    circle.style.opacity = number;
+  }
 
   useEffect(() => {
-    const section = document.querySelector(".section-with-bg")
-    const sectionBis = document.querySelector(".section-with-no-bg")
-    if (count > prevCount) {
-      section.style.transform = "rotate(-8deg)"
-      sectionBis.style.transform = "rotate(-8deg)"
-    } else if (count < prevCount) {
-      section.style.transform = "rotate(8deg)"
-      sectionBis.style.transform = "rotate(8deg)"
-    }
-  }, [count, prevCount])
-
-  useEffect(() => {
-    window.addEventListener("mousemove", setCursorMove, false);
-    return () => window.removeEventListener("mousemove", setCursorMove, false);
+    const container = document.querySelector(".app");
+    container.addEventListener("mousemove", setCursor, false);
+    return () => container.removeEventListener("mousemove", setCursor, false);
   }, []);
 
-  // useEffect(() => {
-  //   document.addEventListener("scroll", setCursorScroll, true);
-  //   return () => document.removeEventListener("scroll", setCursorScroll, true);
-  // }, []);
+  useEffect(() => {
+    const container = document.getElementById("container");
+    container.addEventListener("mousemove", setCursorMove, false);
+    return () => container.removeEventListener("mousemove", setCursorMove, false);
+  }, []);
+
+  useEffect(() => {
+    const container = document.getElementById("container");
+    const circle = document.querySelector(".circle");
+    container.addEventListener("pointerenter", () => { toggleCursor(circle, 1) });
+    return () => container.addEventListener("pointerenter", () => { toggleCursor(circle, 1) });
+  }, []);
+
+  useEffect(() => {
+    const container = document.getElementById("container");
+    const circle = document.querySelector(".circle");
+    container.addEventListener("pointerleave", () => { toggleCursor(circle, 0) });
+    container.addEventListener("pointerleave", () => { toggleCursor(circle, 0) });
+  }, []);
 
   return (
-    <>
-      <div id="cursor" />
-      <div id="colored-cursor" />
+    <div className="app">
       <div className="blue-block" />
       <div className="red-block" />
       <div className="yellow-block" />
-      <i className="fas fa-long-arrow-alt-down" />
-      <div className="app-layout">
-        <div className="header">
-          <div className="logo">LoveForGreece</div>
-        </div>
-        <div className="hero-scene">
-          <h1>Visit Santorini</h1>
-        </div>
-        
-        <section className="section-with-bg">
-          hi
-        </section>
-        <section className="section-with-no-bg">
-          hi
-        </section>
-        <img src="/home.jpg" alt="santorini-home" className="image-block" />
 
+      <div className="circle-small"></div>
+      <div className="header"></div>
+
+      <div id="container">
+        <div className="circle"></div>
+        <div className="message">Visit Santorini</div>
+        <div className="message-stroke">- The Cyclades & the Egee sea -</div>
       </div>
-    </>
+
+      <div id="picture">
+        <div className="message-stroke">Hello</div>
+      </div>
+
+      <div id="container">
+        <div className="message-stroke">Hello</div>
+      </div>
+    </div>
   );
 }
 
