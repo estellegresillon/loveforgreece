4ac5db5ad5f2a495019c38886ba741f4b1da2429
Estelle GRESILLON <estelle.gresillon@gmail.com>
2020-01-13 15:42:11 +0100

    added responsive

--- src/components/sections/sections.js
+++ src/components/sections/sections.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import "./sections.scss";
+import "./sections-mobile.scss";
 
 const Sections = () => {
   const [isSectionOneVisible, setSectionOneVisible] = useState(false);
