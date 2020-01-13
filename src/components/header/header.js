2ef376badbff11212804f24c13f939d9c2e4027b
Estelle GRESILLON <estelle.gresillon@gmail.com>
2020-01-13 11:02:36 +0100

    fixed pictures sizes and added header links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,25 +2,36 @@ import React from "react";
 
 import "./header.scss";
 
-const Header = () => (
-  <div className="header">
-    <div className="logo">
-      <span>
-        <i className="fas fa-sun" />
-        <span style={{ fontWeight: 900 }}>DIVE</span>
-        <span>IN</span>
-      </span>
-      SANTORINI
-    </div>
-    <div className="navigation">
-      <div className="nav-item">
-        WHAT TO DO
+const Header = () => {
+  const goToSection = (section) => {
+    const sectionOne = document.getElementById("section-one");
+    const sectionThree = document.getElementById("section-three");
+
+    if (section === "one") {
+      sectionOne.scrollIntoView();
+    } else sectionThree.scrollIntoView();
+  }
+
+  return (
+    <div className="header">
+      <div className="logo">
+        <span>
+          <i className="fas fa-sun" />
+          <span style={{ fontWeight: 900 }}>DIVE</span>
+          <span>IN</span>
+        </span>
+        SANTORINI
       </div>
-      <div className="nav-item">
-        GET THE GUIDE
+      <div className="navigation">
+        <div onClick={() => goToSection("one")} className="nav-item">
+          WHAT TO DO
+        </div>
+        <div onClick={() => goToSection("three")} className="nav-item">
+          GET THE GUIDE
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </div >
+  );
+};
 
 export default Header;
--- src/components/sections/sections.js
+++ src/components/sections/sections.js
@@ -36,72 +36,73 @@ const Sections = () => {
   }, [])
 
   return (
-  <>
-    <div id="section-one">
-      <div className="image-locations">
-        <div className="locations location-left">
-          <div className="location-text">Fira</div>
-          <ul className="location-description">
-            <li>Climb the Caldeira</li>
-            <li>Catch the cable car</li>
-            <li>Eat gyros</li>
-            <li>Three bells of Fira</li>
-          </ul>
-        </div>
+    <>
+      <div id="section-one">
+        <div className="image-locations">
+          <div className="locations location-left">
+            <div className="location-text">Fira</div>
+            <ul className="location-description">
+              <li>Climb the Caldeira</li>
+              <li>Catch the cable car</li>
+              <li>Eat gyros</li>
+              <li>Three bells of Fira</li>
+            </ul>
+          </div>
 
-        <div className="locations location-center">
-          <div className="location-text">Oia</div>
-          <ul className="location-description">
-            <li>Byzantine castle ruins</li>
-            <li>Ammoudi</li>
-            <li>Oia Castle</li>
-            <li>Maritime museum</li>
-          </ul>
-        </div>
+          <div className="locations location-center">
+            <div className="location-text">Oia</div>
+            <ul className="location-description">
+              <li>Byzantine castle ruins</li>
+              <li>Ammoudi</li>
+              <li>Oia Castle</li>
+              <li>Maritime museum</li>
+            </ul>
+          </div>
 
-        <div className="locations location-right">
-          <div className="location-text">Thera</div>
-          <ul className="location-description">
-            <li>Archeological museum</li>
-            <li>Walk through Thera</li>
-            <li>Akrotiri ruins</li>
-            <li>Watch the sunset</li>
-          </ul>
+          <div className="locations location-right">
+            <div className="location-text">Thera</div>
+            <ul className="location-description">
+              <li>Archeological museum</li>
+              <li>Walk through Thera</li>
+              <li>Akrotiri ruins</li>
+              <li>Watch the sunset</li>
+            </ul>
+          </div>
         </div>
+        {isSectionOneVisible && <div className="message-stroke">what to see, what to do</div>}
       </div>
-      {isSectionOneVisible && <div className="message-stroke">what to see, what to do</div>}
-    </div>
 
-    <div id="section-two">
-      {isSectionTwoVisible &&
-        <>
-          <img className="pink-waves" width="200px" src="/pink-waves.svg" alt="wave" />
-          <img className="blue-waves" width="200px" src="/blue-waves.svg" alt="wave" />
-          <div className="message-plain">Discover the deep waters, the breathtaking cliffs, the shiny black sands and the romantic pink skies.</div>
-          <div className="message-stroke">Santorini is a miracle of nature</div>
-        </>}
-    </div>
+      <div id="section-two">
+        {isSectionTwoVisible &&
+          <>
+            <img className="pink-waves" width="200px" src="/pink-waves.svg" alt="wave" />
+            <img className="blue-waves" width="200px" src="/blue-waves.svg" alt="wave" />
+            <div className="message-plain">Discover the deep waters, the breathtaking cliffs, the shiny black sands and the romantic pink skies.</div>
+            <div className="message-stroke">Santorini is a miracle of nature</div>
+          </>}
+      </div>
 
-    <div id="section-three">
-      {isSectionThreeVisible && 
-        <>
-          <div className="left-section">
-            <img src="/home2.jpg" alt="guide-image" />
-          </div>
-          <div className="right-section">
-            <div className="message-stroke">Get your guide :</div>
-            <div className="message-plain">Receive a free guide for your tour in the cyclades directly in your email box</div>
-            <div className="guide-form">
-              <label>Name</label>
-              <input type="text"></input>
-              <label>e-mail</label>
-              <input type="text"></input>
-              <button type="submit">GET THE GUIDE</button>
+      <div id="section-three">
+        {isSectionThreeVisible &&
+          <>
+            <div className="left-section">
+              <img src="/home2-min.jpg" alt="guide-overview" />
             </div>
-          </div>
-        </>}
-    </div>
-  </>
-);}
+            <div className="right-section">
+              <div className="message-stroke">Get your guide :</div>
+              <div className="message-plain">Receive a free guide for your tour in the cyclades directly in your email box</div>
+              <div className="guide-form">
+                <label>Name</label>
+                <input type="text"></input>
+                <label>e-mail</label>
+                <input type="text"></input>
+                <button type="submit">GET THE GUIDE</button>
+              </div>
+            </div>
+          </>}
+      </div>
+    </>
+  );
+}
 
 export default Sections;
