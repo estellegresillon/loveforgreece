da4b0039a93b90ddf430f728252d86d5d350d274
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-22 23:41:45 +0100

    refacto hero scene

--- src/components/hero-scene/hero-scene.js
+++ src/components/hero-scene/hero-scene.js
@@ -6,23 +6,21 @@ const HeroScene = () => (
   <>
     <div id="hero-scene">
       <div className="cursor"></div>
-      <div className="message-pink">WELCOME TO SANTORINI</div>
-      <div className="title">The Cyclades & the Egee sea</div>
-      <div className="subtitle">
-        Visit our islands
+      
+      <div className="hero-scene-text">
+        <div className="message-pink">WELCOME TO SANTORINI</div>
+        <div className="title">The Cyclades & the Egee sea</div>
+      </div>
+      <div className="button-go">
+        GO
         <i className="fas fa-arrow-right" />
       </div>
-      <div className="background-water-mask" />
+      <div className="hero-image" />
+      <div className="background">
+        <div className="blue" />
+      </div>
       <div className="background-water" />
     </div>
-    <svg>
-      <filter id="turbulence" x="0" y="0" width="100%">
-        <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0" />
-        <feDisplacementMap scale="20" in="SourceGraphic" />
-        <animate xlinkHref="#sea-filter" attributeName="baseFrequency" dur="60s"
-          keyTimes="0;0.5;1" values="0.02 0.06; 0.04 0.08; 0.02 0.06" repeatCount="indefinite" />
-      </filter>
-    </svg>
   </>
 );
 
