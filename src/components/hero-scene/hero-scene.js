d65d5ca46622718cb108f502a14afef042abf507
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-23 00:10:14 +0100

    added waves to hero scene and fixed section two

--- src/components/hero-scene/hero-scene.js
+++ src/components/hero-scene/hero-scene.js
@@ -2,26 +2,33 @@ import React from "react";
 
 import "./hero-scene.scss";
 
-const HeroScene = () => (
-  <>
-    <div id="hero-scene">
-      <div className="cursor"></div>
-      
-      <div className="hero-scene-text">
-        <div className="message-pink">WELCOME TO SANTORINI</div>
-        <div className="title">The Cyclades & the Egee sea</div>
+const HeroScene = () => {
+  const scrollToSectionOne = () => {
+    document.getElementById("section-one").scrollIntoView();
+  }
+
+  return (
+    <>
+      <div id="hero-scene">
+        <div className="cursor"></div>
+
+        <img className="white-waves" width="200px" src="/white-waves.svg" alt="wave" />
+        <div className="hero-scene-text">
+          <div className="message-pink">WELCOME TO SANTORINI</div>
+          <div className="title">The Cyclades & the Egee sea</div>
+        </div>
+        <div onClick={() => scrollToSectionOne()} className="button-go">
+          GO
+          <i className="fas fa-arrow-right" />
+        </div>
+        <div className="hero-image" />
+        <div className="background">
+          <div className="blue" />
+        </div>
+        <div className="background-water" />
       </div>
-      <div className="button-go">
-        GO
-        <i className="fas fa-arrow-right" />
-      </div>
-      <div className="hero-image" />
-      <div className="background">
-        <div className="blue" />
-      </div>
-      <div className="background-water" />
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default HeroScene;
