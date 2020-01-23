f00cc3056ab34fb35a0525d6897a3d36fce0ffdc
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-23 07:58:37 +0100

    test ping app every 5mn

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './components/app';
 
+const options = { method: 'GET', mode: 'no-cors' };
+
+setInterval(function() {
+  fetch(`http://santorini.estellegresillon.fr`, options)
+}, 300000);
+
 ReactDOM.render(<App />, document.getElementById('root'));
